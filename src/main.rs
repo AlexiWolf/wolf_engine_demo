1d65b214002170960de9abd1ecde9e2a53d88a73
AlexiWolf <alexiwolf@pm.me>
2022-05-06 18:49:24 -0500

    Implemented basic example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::time::{Instant, Duration};
+
+use wolf_engine::*;
+use wolf_engine_sdl2::*;
+use log::*;
+use sdl2::pixels::Color;
+
 fn main() {
-    println!("Hello, world!");
+    logging::initialize_logging(LevelFilter::Debug);    
+    EngineBuilder::new()
+        .with_plugin(Box::from(SdlPlugin::new(SdlWindowSettings::default())))
+        .build()
+        .run(Box::from(MainState::new()));
+}
+
+pub struct MainState {
+    last_color_change: Instant,
+    color: Color,
+}
+
+impl MainState {
+    pub fn new() -> Self {
+        Self {
+            last_color_change: Instant::now(),
+            color: Color::RED,
+        }
+    }
+}
+
+impl State for MainState {
+    fn update(&mut self, _context: &mut Context) -> OptionalTransition {
+        if self.last_color_change.elapsed() > Duration::from_secs(1) {
+            if self.color == Color::RED {
+                self.color = Color::BLUE; 
+            } else {
+                self.color = Color::RED;
+            }
+            self.last_color_change = Instant::now();
+        }
+        None
+    }
+
+    fn render(&mut self, context: &mut Context) -> RenderResult {
+        if let Some(Ok(mut sdl_graphics)) = context.try_borrow_mut::<SdlVideoContext>() {
+            sdl_graphics.canvas.set_draw_color(self.color);
+            sdl_graphics.canvas.clear();
+            sdl_graphics.canvas.present();
+        }
+    }
 }
